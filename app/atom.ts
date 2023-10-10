d2fa12d00347a454931999a2ed551d40c727b501
mitsuYashi <hm20011027@gmail.com>
2023-10-10 20:44:09 +0900

    chore: firebase & recoil

---
+++ app/atom.ts
@@ -0,0 +1,46 @@
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { atom } from 'recoil';
+import { auth } from '@/lib/firebase';
+
+export const firebaseUserIdState = atom<string | undefined>({
+  key: 'firebaseUserIdState',
+  default: undefined,
+  effects: [
+    ({ setSelf }) => onAuthStateChanged(auth, (user) => setSelf(user?.uid)),
+  ],
+});
+
+type FirebaseUser = Pick<User, 'photoURL' | 'displayName' | 'email'>;
+
+export const firebaseUserState = atom<FirebaseUser | undefined>({
+  key: 'firebaseUserState',
+  default: undefined,
+  effects: [
+    ({ setSelf }) => {
+      return onAuthStateChanged(auth, async (user) => {
+        if (!user) {
+          return;
+        }
+        const data = {
+          photoURL: user.photoURL,
+          displayName: user.displayName,
+          email: user.email,
+        };
+        setSelf(data);
+      });
+    },
+  ],
+});
+
+export const firebaseTokenState = atom<string | null>({
+  key: 'firebaseTokenState',
+  default: null,
+  effects: [
+    ({ setSelf }) => {
+      return onAuthStateChanged(auth, async (user) => {
+        if (!user) return;
+        setSelf(await user.getIdToken());
+      });
+    },
+  ],
+});
---
+++ lib/firebase.ts
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC8l-TLm386qc5roi8_JaM8cBiD-SrWuoU',
+  authDomain: 'misotenb.firebaseapp.com',
+  projectId: 'misotenb',
+  storageBucket: 'misotenb.appspot.com',
+  messagingSenderId: '613896956187',
+  appId: '1:613896956187:web:f248b3bafda04887204bbe',
+  measurementId: 'G-8VBG2K6KXR',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+auth.languageCode = 'it';
+
+export const firebaseSignIn = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential!.accessToken;
+      const user = result.user;
+    })
+    .catch((error) => {});
+};
+
+export const firebaseSignOut = () => {
+  signOut(auth)
+    .then(() => {})
+    .catch((error) => {
+      console.log(error);
+    });
+};
