65093578ab086d0e925e735cf752ca3f3e822404
hkiku482 <h.kikuchi482@gmail.com>
2023-10-16 02:44:15 +0000

    chore: layoutに移動

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import { ConfigProvider } from 'antd';
 import { Inter } from 'next/font/google';
 import StyledComponentsRegistry from './_shared/lib/AntdRegistry';
+import theme from './_shared/theme/themeConfig';
 import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,9 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <ConfigProvider theme={theme}>{children}</ConfigProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import { ConfigProvider } from 'antd';
 import { Button } from './_components/common/Button';
-import theme from './_shared/theme/themeConfig';
 
 export default function Home() {
-  return (
-    <ConfigProvider theme={theme}>
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    </ConfigProvider>
-  );
+  return <Button type="primary">Button</Button>;
 }
