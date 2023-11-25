c34903134b40af18f848eba8a24021dc8c2faa10
mitsuYashi <hm20011027@gmail.com>
2023-11-25 18:08:39 +0900

    change: text to title

--- src/app/register/_components/Register.tsx
+++ src/app/register/_components/Register.tsx
@@ -1,14 +1,13 @@
 'use client';
-import { Text } from '@mantine/core';
 import Link from 'next/link';
 import { RegisterCard } from './RegisterCard';
 import { Anchor } from '@/shared/components/common/Anchor';
 import { Divider } from '@/shared/components/common/Divider';
 import { Flex } from '@/shared/components/common/Layout';
 import { Center } from '@/shared/components/common/Layout/Center';
-import { Space } from '@/shared/components/common/Layout/Space';
 import { Stack } from '@/shared/components/common/Layout/Stack';
 
+import { Title } from '@/shared/components/common/Title';
 import { GoogleButton } from '@/shared/components/features/GoogleButton';
 import { AirshipMainVisual } from '@/shared/components/features/MainVisual';
 
@@ -17,11 +16,10 @@ export const Register = () => {
     <>
       <Flex align="center" direction="column">
         <AirshipMainVisual maw={800} />
-        <Space h="lg" />
-        <Stack gap="lg" justify="center">
-          <Text size="md" fw={500} ta="center">
+        <Stack gap="lg" justify="center" mt="lg">
+          <Title order={3} ta="center">
             新規登録
-          </Text>
+          </Title>
           <RegisterCard />
           <Divider label="または" m="lg" />
 
