2e1eadd9d1800896919613d7b4f56ed1cb273b84
Yutaka Sadohara <yutakasadohara@YutakanoMacBook-Pro.local>
2023-12-01 00:36:56 +0900

    add: 3modelの選択ui

--- src/app/ar_assets/create/_components/StepContent/Select3dModel.tsx
+++ src/app/ar_assets/create/_components/StepContent/Select3dModel.tsx
@@ -1,3 +1,78 @@
+import { Container } from '@/shared/components/common/Container';
+import { Divider } from '@/shared/components/common/Divider';
+import { Image } from '@/shared/components/common/Image';
+import { FileInput } from '@/shared/components/common/Input/FileInput';
+import { Radio } from '@/shared/components/common/Input/Radio';
+import { Grid, Stack } from '@/shared/components/common/Layout';
+import { Text } from '@/shared/components/common/Text';
+import { Title } from '@/shared/components/common/Title';
+
 export const Select3dModel = () => {
-  return <>3Dモデルの選択</>;
+  return (
+    <Container>
+      <Title order={3} c="blue.6" mb={16}>
+        3Dモデルの選択
+      </Title>
+      <Title order={5} c="blue.6" mb={4}>
+        サンプル3Dモデル
+      </Title>
+      <Text size="xs" c="gray.6" mb={12}>
+        用意できる3Dモデルがない場合はサンプルからお選びください
+      </Text>
+
+      <Grid gutter="sm">
+        <Grid.Col span={4}>
+          <Stack gap="sm" align="center">
+            <Image src="/3dmodel_image.svg" alt="#"></Image>
+            <Radio size="xs" />
+          </Stack>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Stack gap="sm" align="center">
+            <Image src="/3dmodel_image.svg" alt="#"></Image>
+            <Radio size="xs" />
+          </Stack>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Stack gap="sm" align="center">
+            <Image src="/3dmodel_image.svg" alt="#"></Image>
+            <Radio size="xs" />
+          </Stack>
+        </Grid.Col>
+      </Grid>
+      <Divider my="sm" labelPosition="center"></Divider>
+
+      <Title order={5} c="blue.6" mb={4}>
+        アップロードした3Dモデル
+      </Title>
+      <Text size="xs" c="gray.6" mb={12}>
+        自分でアップロードした3Dモデルを選択するにはアップロードする必要があります
+      </Text>
+      <Grid>
+        <Grid.Col span={4}>
+          <Stack gap="sm" align="center">
+            <Image src="/3dmodel_image.svg" alt="#"></Image>
+            <Radio size="xs" />
+          </Stack>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <Stack gap="sm" align="center">
+            <Image src="/3dmodel_image.svg" alt="#"></Image>
+            <Radio size="xs" />
+          </Stack>
+        </Grid.Col>
+        <Grid.Col span={4}>
+          <FileInput
+            placeholder="アップロード"
+            size="xs"
+            styles={{
+              wrapper: { height: '100%' },
+              root: { height: '100%' },
+              input: { height: 'calc(100% - 28px)' },
+            }}
+          ></FileInput>
+        </Grid.Col>
+      </Grid>
+    </Container>
+  );
 };
