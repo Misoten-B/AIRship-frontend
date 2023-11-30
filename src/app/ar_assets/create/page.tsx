bb4c01c95d022519cb30f6eb277fefb8e2fc2809
honoka2001 <honoka519pk@gmail.com>
2023-11-30 17:17:03 +0900

    コメントを削除

--- src/app/ar_assets/create/page.tsx
+++ src/app/ar_assets/create/page.tsx
@@ -1,13 +1,7 @@
 import { CreateArAssetStepper } from './_components/CreateArAssetStepper';
-// import { Guard } from '@/shared/components/features';
 
 const Page = () => {
-  return (
-    // MEMO: Stepper関連のUI実装が終わったら消しておく
-    // <Guard>
-    <CreateArAssetStepper />
-    // </Guard>
-  );
+  return <CreateArAssetStepper />;
 };
 
 export default Page;
