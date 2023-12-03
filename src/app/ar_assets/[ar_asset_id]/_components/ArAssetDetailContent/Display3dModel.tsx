5344cd0a9088118322086688c09f337cf35c4658
honoka2001 <honoka519pk@gmail.com>
2023-12-03 22:57:24 +0900

    style: ARAsset詳細画面のUI

---
+++ src/app/ar_assets/[ar_asset_id]/_components/ArAssetDetailContent/Display3dModel.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { Button } from '@/shared/components/common/Button';
+import { Image } from '@/shared/components/common/Image';
+import { Group, Stack } from '@/shared/components/common/Layout';
+import { Text } from '@/shared/components/common/Text';
+import { Title } from '@/shared/components/common/Title';
+
+export const Display3dModel = () => {
+  return (
+    <Stack gap={0}>
+      <Title order={5} c="blue.6" mb={4}>
+        3Dモデルデータ
+      </Title>
+      <Text size="xs" c="gray.6" mb={12}>
+        3Dモデルデータを登録すると、QRコードから3DモデルをARとして表示させることができます。
+      </Text>
+      <Group justify="center" mt={8}>
+        <Image src="/3dmodel_image.svg" alt="#" height={160} />
+        <Button variant="outline" color="orange" radius="xl">
+          選択する
+        </Button>
+      </Group>
+    </Stack>
+  );
+};
