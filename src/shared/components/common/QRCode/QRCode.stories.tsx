11e8cefc6c7e4e2715c2bba6ff99200072b2f4bb
mitsuYashi <hm20011027@gmail.com>
2023-12-11 12:38:32 +0900

    change: image必須ではないように

---
+++ src/shared/components/common/QRCode/QRCode.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { QRCode } from '.';
+
+const meta: Meta<typeof QRCode> = {
+  component: undefined,
+};
+export default meta;
+
+type Story = StoryObj<typeof QRCode>;
+
+export const Main: Story = {
+  render: () => (
+    <QRCode
+      url={'https://airship.azurewebsites.net/'}
+      imageSrc="/logo.svg"
+      size={50}
+    ></QRCode>
+  ),
+};
