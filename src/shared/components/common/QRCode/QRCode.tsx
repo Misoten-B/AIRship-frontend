0724fd78d9a06e84ddce1e6c37ad00e2fcdcb49b
mitsuYashi <hm20011027@gmail.com>
2023-12-11 12:38:27 +0900

    change: image必須ではないように

--- src/shared/components/common/QRCode/QRCode.tsx
+++ src/shared/components/common/QRCode/QRCode.tsx
@@ -3,7 +3,7 @@ import { Center, Paper } from '../Layout';
 
 type Props = {
   url: string;
-  imageSrc: string;
+  imageSrc?: string;
   size: number;
 };
 export const QRCode = (props: Props) => {
@@ -17,14 +17,16 @@ export const QRCode = (props: Props) => {
           bgColor={'#ffffff'}
           fgColor={'#000000'}
           level={'L'}
-          imageSettings={{
-            src: imageSrc,
-            x: undefined,
-            y: undefined,
-            height: size / 4.5,
-            width: size / 4.5,
-            excavate: true,
-          }}
+          {...(imageSrc && {
+            imageSettings: {
+              src: imageSrc,
+              x: undefined,
+              y: undefined,
+              height: size / 4.5,
+              width: size / 4.5,
+              excavate: true,
+            },
+          })}
         />
       </Paper>
     </Center>
