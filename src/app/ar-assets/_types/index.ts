5004c618b4f4cf9e2671ac8ee113229880deb541
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-14 12:08:57 +0900

    feat: ARアセットのQRコードをRequiredから除外

--- src/app/ar-assets/_types/index.ts
+++ src/app/ar-assets/_types/index.ts
@@ -1,3 +1,5 @@
 import { Dto_ArAssetsResponse } from '@/api/@types';
 
-export type ARAsset = Required<Dto_ArAssetsResponse>;
+type SomeRequired<T, K extends keyof T> = T & Required<Omit<T, K>>;
+
+export type ARAsset = SomeRequired<Dto_ArAssetsResponse, 'qrcode_image_path'>;
