6dc81f7217d064b89db4c8c4f6e8e1443707e133
mitsuYashi <hm20011027@gmail.com>
2023-12-14 19:59:38 +0900

    feat: api 連携
    Co-authored-by: hkiku482 <hkiku482@users.noreply.github.com>

---
+++ src/shared/components/features/LoginGoogleButton.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { notifications } from '@mantine/notifications';
+import { useRouter } from 'next/navigation';
+import { useCallback } from 'react';
+import { useRecoilValue } from 'recoil';
+import { GoogleButton } from '../common/Button';
+import { ErrorNotificationData } from '../common/Feedback/ErrorNotification';
+import { useAuth } from '@/shared/hooks/auth';
+import { useGetUser } from '@/shared/hooks/restapi/v1/User';
+import { useApiClient } from '@/shared/lib/axios/AxiosProvider';
+import { firebaseUserState } from '@/shared/lib/recoil';
+
+export const LoginGoogleButton = () => {
+  const router = useRouter();
+  const firebaseUser = useRecoilValue(firebaseUserState);
+  const { data, mutate, error } = useGetUser(!firebaseUser?.token);
+  const { login, logout } = useAuth();
+  const { api } = useApiClient();
+
+  const handleClick = useCallback(async () => {
+    try {
+      login && (await login());
+      const d = await mutate();
+
+      if (d) {
+        console.debug('data', d);
+        router.push('/cards');
+      } else {
+        throw true;
+      }
+    } catch (error) {
+      notifications.show(ErrorNotificationData('Error', '登録されていません'));
+      logout && (await logout());
+    }
+  }, [login, logout, mutate, router]);
+
+  return <GoogleButton onClick={handleClick} />;
+};
