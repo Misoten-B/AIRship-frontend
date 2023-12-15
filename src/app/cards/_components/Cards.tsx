474a71882d0b80b90ab5935842972ab5d1a72842
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-15 15:58:45 +0900

    feat: 名刺一覧仮呼び出し、要動作確認＆改善

--- src/app/cards/_components/Cards.tsx
+++ src/app/cards/_components/Cards.tsx
@@ -1,30 +1,55 @@
 'use client';
-import { useState } from 'react';
+
+import { useMemo, useState } from 'react';
 import { Button } from '@/shared/components/common/Button';
 import { Container } from '@/shared/components/common/Container';
 import { Center, Flex } from '@/shared/components/common/Layout';
+import { Loader } from '@/shared/components/common/Loader';
 import { BusinessCard } from '@/shared/components/features';
 import { BusinessCardAspectRatio } from '@/shared/components/features/BusinessCard/BusinessCardAspectRatio';
+import { useGetBusinessCards } from '@/shared/hooks/restapi/v1/BusinessCard';
 import { useMediaQuery } from '@/shared/hooks/useMediaQuery';
 
-const cardData = [
-  { id: 1, name: '1' },
-  { id: 2, name: '2' },
-  { id: 3, name: '3' },
-];
+const defaultBusinessCardWidht = 379;
+const diffTop = 120;
+
+type State = {
+  selectedCard: number;
+};
+
+const initialState: State = {
+  selectedCard: 0,
+};
 
 export const Cards = () => {
+  const { data, error, isLoading } = useGetBusinessCards();
+
   const isPC = useMediaQuery('(min-width: 768px)');
-  const defaultBusinessCardWidht = 379;
-  const [selectedCard, setSelectedCard] = useState<number>(1);
-  const diffTop = 120;
-  const ButtonTop = diffTop * (cardData.length + 1) + 40;
-  const height = ButtonTop + 200;
+  const [selectedCard, setSelectedCard] = useState(initialState.selectedCard);
+
+  // const ButtonTop = diffTop * (cardData.length + 1) + 40;
+  // const height = ButtonTop + 200;
+
+  // 計算が重くないから必要ないかも
+  const ButtonTop = useMemo(() => {
+    const length = data?.length ?? 0;
+    return diffTop * (length + 1) + 40;
+  }, [data]);
+
+  const height = useMemo(() => {
+    return ButtonTop + 40;
+  }, [ButtonTop]);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <Loader />;
+
+  if (!data) return null;
+
   return isPC ? (
     <Container w={'100%'}>
       <Flex w="100%" wrap="wrap" gap="lg" justify="space-around">
-        {cardData.map((card, index) => (
-          <BusinessCard key={card.id} text={card.name} />
+        {data.map((card) => (
+          <BusinessCard key={card.id} text={card.displayName ?? '名無し'} />
         ))}
         <BusinessCardAspectRatio w={defaultBusinessCardWidht}>
           <Button variant="default">+</Button>
@@ -34,16 +59,17 @@ export const Cards = () => {
   ) : (
     <Container mt={80} pos="relative" h={height}>
       <Center>
-        {cardData.map((card, index) => {
+        {data.map((card, index) => {
           const top = index * diffTop;
           return (
             <BusinessCard
               key={card.id}
-              text={card.name}
+              text={card.displayName ?? '名無し'}
               style={{
-                zIndex: cardData.length + index,
+                zIndex: data.length + index,
                 transform:
-                  selectedCard === card.id
+                  // selectedCard === card.id
+                  selectedCard === 1
                     ? 'translate(-50%, -80px)'
                     : selectedCard > index
                     ? 'translate(-50%, -40px)'
@@ -54,7 +80,8 @@ export const Cards = () => {
                 top: top,
                 left: '50%',
               }}
-              onClick={() => setSelectedCard(card.id)}
+              // onClick={() => setSelectedCard(card.id)}
+              onClick={() => setSelectedCard(1)}
             />
           );
         })}
