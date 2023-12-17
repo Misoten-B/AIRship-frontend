42908589b61ff05748bbdc6708ba6f709da8a38a
mitsuYashi <hm20011027@gmail.com>
2023-12-17 17:52:58 +0900

    feat: coordinate

--- src/shared/components/features/BusinessCard/BusinessCard.tsx
+++ src/shared/components/features/BusinessCard/BusinessCard.tsx
@@ -2,22 +2,92 @@
 import { AspectRatioProps, CardProps } from '@mantine/core';
 import { useElementSize } from '@mantine/hooks';
 import React, { useEffect, useState } from 'react';
-import { Card, Flex, Stack } from '../../common/Layout';
+import { Card } from '../../common/Layout';
 import { QRCode } from '../../common/QRCode';
 import { Text } from '../../common/Text';
 import { BusinessCardAspectRatio } from './BusinessCardAspectRatio';
+import {
+  Dto_BusinessCardPartsCoordinate,
+  Dto_BusinessCardResponse,
+} from '@/api/@types';
 
 type OmitAspectRatioProps = Omit<AspectRatioProps, 'ratio' | 'w'>;
 type OmitCardProps = Omit<CardProps, 'shadow' | 'padding' | 'withBorder'>;
 type Props = OmitAspectRatioProps & {
-  text: string;
+  card: Dto_BusinessCardResponse;
   handleClick?: () => void;
 };
 
-export const BusinessCard = ({ text, ...props }: Props) => {
+const scaleRatio = 3;
+const defaultWidth = 1254 / scaleRatio;
+const fontSize = {
+  displayName: 24,
+  companyName: 16,
+  department: 14,
+  officialPosition: 12,
+  phoneNumber: 12,
+  email: 12,
+  postalCode: 12,
+  address: 12,
+};
+
+export const BusinessCard = ({ card, handleClick, ...props }: Props) => {
   const { ref, width, height } = useElementSize();
   const [scale, setScale] = useState(1);
-  const defaultWidth = 379;
+
+  const rawBusinessCardPartsCoordinate = {
+    displayNameX: 112,
+    displayNameY: 266,
+    companyNameX: 116,
+    companyNameY: 98,
+    departmentX: 116,
+    departmentY: 152,
+    officialPositionX: 116,
+    officialPositionY: 200,
+    phoneNumberX: 116,
+    phoneNumberY: 478,
+    emailX: 116,
+    emailY: 428,
+    postalCodeX: 116,
+    postalCodeY: 574,
+    addressX: 116,
+    addressY: 614,
+    qrcodeX: 760,
+    qrcodeY: 209,
+  };
+
+  const businessCardPartsCoordinate: Dto_BusinessCardPartsCoordinate = {
+    displayNameX:
+      (rawBusinessCardPartsCoordinate.displayNameX / scaleRatio) * scale,
+    displayNameY:
+      (rawBusinessCardPartsCoordinate.displayNameY / scaleRatio) * scale,
+    companyNameX:
+      (rawBusinessCardPartsCoordinate.companyNameX / scaleRatio) * scale,
+    companyNameY:
+      (rawBusinessCardPartsCoordinate.companyNameY / scaleRatio) * scale,
+    departmentX:
+      (rawBusinessCardPartsCoordinate.departmentX / scaleRatio) * scale,
+    departmentY:
+      (rawBusinessCardPartsCoordinate.departmentY / scaleRatio) * scale,
+    officialPositionX:
+      (rawBusinessCardPartsCoordinate.officialPositionX / scaleRatio) * scale,
+    officialPositionY:
+      (rawBusinessCardPartsCoordinate.officialPositionY / scaleRatio) * scale,
+    phoneNumberX:
+      (rawBusinessCardPartsCoordinate.phoneNumberX / scaleRatio) * scale,
+    phoneNumberY:
+      (rawBusinessCardPartsCoordinate.phoneNumberY / scaleRatio) * scale,
+    emailX: (rawBusinessCardPartsCoordinate.emailX / scaleRatio) * scale,
+    emailY: (rawBusinessCardPartsCoordinate.emailY / scaleRatio) * scale,
+    postalCodeX:
+      (rawBusinessCardPartsCoordinate.postalCodeX / scaleRatio) * scale,
+    postalCodeY:
+      (rawBusinessCardPartsCoordinate.postalCodeY / scaleRatio) * scale,
+    addressX: (rawBusinessCardPartsCoordinate.addressX / scaleRatio) * scale,
+    addressY: (rawBusinessCardPartsCoordinate.addressY / scaleRatio) * scale,
+    qrcodeX: (rawBusinessCardPartsCoordinate.qrcodeX / scaleRatio) * scale,
+    qrcodeY: (rawBusinessCardPartsCoordinate.qrcodeY / scaleRatio) * scale,
+  };
 
   useEffect(() => {
     if (width && height) {
@@ -32,12 +102,12 @@ export const BusinessCard = ({ text, ...props }: Props) => {
       ref={ref}
       style={{
         cursor: 'pointer',
+        transform: `scale(${scale})`,
       }}
       {...props}
     >
       <Card
         shadow="sm"
-        padding="lg"
         withBorder
         w="100%"
         h="100%"
@@ -45,16 +115,112 @@ export const BusinessCard = ({ text, ...props }: Props) => {
           transform: `scale(${scale})`,
         }}
       >
-        <Flex w="100%" h="100%" justify="space-around">
-          <Stack>
-            <Text size="lg">{text}</Text>
-          </Stack>
-          <QRCode
-            url={'https://airship.azurewebsites.net/'}
-            imageSrc="/airship-logo-column.svg"
-            size={80 * scale}
-          />
-        </Flex>
+        <Text
+          style={{
+            fontSize: fontSize.displayName * scale,
+          }}
+          mb="md"
+          pos="absolute"
+          fw={700}
+          top={businessCardPartsCoordinate?.displayNameY}
+          left={businessCardPartsCoordinate?.displayNameX}
+        >
+          {card.displayName}
+        </Text>
+        <Text
+          style={{
+            fontSize: fontSize.companyName * scale,
+          }}
+          mb="md"
+          pos="absolute"
+          top={businessCardPartsCoordinate?.companyNameY}
+          left={businessCardPartsCoordinate?.companyNameX}
+        >
+          {card.companyName}
+        </Text>
+        <Text
+          style={{
+            fontSize: fontSize.department * scale,
+          }}
+          size="sm"
+          mb="md"
+          pos="absolute"
+          top={businessCardPartsCoordinate?.departmentY}
+          left={businessCardPartsCoordinate?.departmentX}
+        >
+          {card.department}
+        </Text>
+        <Text
+          style={{
+            fontSize: fontSize.officialPosition * scale,
+          }}
+          size="xs"
+          mb="md"
+          pos="absolute"
+          top={businessCardPartsCoordinate?.officialPositionY}
+          left={businessCardPartsCoordinate?.officialPositionX}
+        >
+          {card.officialPosition}
+        </Text>
+        <Text
+          style={{
+            fontSize: fontSize.phoneNumber * scale,
+          }}
+          size="sm"
+          mb="md"
+          pos="absolute"
+          top={businessCardPartsCoordinate?.phoneNumberY}
+          left={businessCardPartsCoordinate?.phoneNumberX}
+        >
+          {card.phoneNumber}
+        </Text>
+        <Text
+          style={{
+            fontSize: fontSize.email * scale,
+          }}
+          size="sm"
+          mb="md"
+          pos="absolute"
+          top={businessCardPartsCoordinate?.emailY}
+          left={businessCardPartsCoordinate?.emailX}
+        >
+          {card.email}
+        </Text>
+        <Text
+          style={{
+            fontSize: fontSize.postalCode * scale,
+          }}
+          size="sm"
+          mb="md"
+          pos="absolute"
+          top={businessCardPartsCoordinate?.postalCodeY}
+          left={businessCardPartsCoordinate?.postalCodeX}
+        >
+          〒{card.postalCode}
+        </Text>
+        <Text
+          style={{
+            fontSize: fontSize.address * scale,
+          }}
+          size="sm"
+          mb="md"
+          pos="absolute"
+          top={businessCardPartsCoordinate?.addressY}
+          left={businessCardPartsCoordinate?.addressX}
+        >
+          {card.address}
+        </Text>
+
+        <QRCode
+          url={'https://airship.azurewebsites.net/'}
+          imageSrc="/airship-logo-column.svg"
+          size={90 * scale}
+          style={{
+            position: 'absolute',
+            top: businessCardPartsCoordinate?.qrcodeY,
+            left: businessCardPartsCoordinate?.qrcodeX,
+          }}
+        />
       </Card>
     </BusinessCardAspectRatio>
   );
