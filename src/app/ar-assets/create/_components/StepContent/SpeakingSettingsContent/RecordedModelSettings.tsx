ae356b45affde34602d314883cdb43659778fe9a
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-17 11:30:42 +0900

    feat: FFmpegによるwebm -> wav変換、動作確認中

--- src/app/ar-assets/create/_components/StepContent/SpeakingSettingsContent/RecordedModelSettings.tsx
+++ src/app/ar-assets/create/_components/StepContent/SpeakingSettingsContent/RecordedModelSettings.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { fetchFile, toBlobURL } from '@ffmpeg/util';
 import { useEffect, useRef } from 'react';
 import { ActionIcon, Button } from '@/shared/components/common/Button';
 import { Container } from '@/shared/components/common/Container';
@@ -13,6 +15,8 @@ const MAX_RECORDING_TIME = 10;
 
 export const RecordedModelSettings = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const ffmpegRef = useRef(new FFmpeg());
+
   const {
     isRecording,
     recordingTime,
@@ -31,9 +35,46 @@ export const RecordedModelSettings = () => {
   useEffect(() => {
     if (!recordingBlob) return;
 
-    // 録音データのセットアップ
-    const audio = audioRef.current!;
-    audio.src = URL.createObjectURL(recordingBlob);
+    (async () => {
+      // FFmpegのセットアップ
+      const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd';
+      const ffmpeg = ffmpegRef.current;
+
+      await ffmpeg.load({
+        coreURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.js`,
+          'text/javascript',
+        ),
+        wasmURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.wasm`,
+          'application/wasm',
+        ),
+      });
+
+      // 録音データの変換
+      const inputFileName = 'input.webm';
+      const outputFileName = 'output.wav';
+
+      const file = new File([recordingBlob], inputFileName, {
+        type: recordingBlob.type,
+        lastModified: Date.now(),
+      });
+
+      await ffmpeg.writeFile(inputFileName, await fetchFile(file));
+      await ffmpeg.exec(['-i', inputFileName, outputFileName]);
+      const output = await ffmpeg.readFile(outputFileName);
+
+      const wavBlob = new Blob([output], { type: 'audio/wav' });
+
+      // 録音データのセットアップ
+      const audio = audioRef.current!;
+      audio.src = URL.createObjectURL(wavBlob);
+
+      // const link = document.createElement('a');
+      // link.href = audio.src;
+      // link.download = 'recorded.wav';
+      // link.click();
+    })();
   }, [recordingBlob]);
 
   const handleToggleRecording = () => {
