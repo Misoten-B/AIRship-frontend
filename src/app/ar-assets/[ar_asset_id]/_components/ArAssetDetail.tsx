9bf1a2d0321717878ff1042ba4f1a6220cceed2e
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-22 14:06:49 +0900

    feat: コンポーネントをPNGに変換するフックの実装

--- src/app/ar-assets/[ar_asset_id]/_components/ArAssetDetail.tsx
+++ src/app/ar-assets/[ar_asset_id]/_components/ArAssetDetail.tsx
@@ -1,5 +1,3 @@
-import { toPng } from 'html-to-image';
-import { useCallback, useRef } from 'react';
 import {
   Display3dModel,
   QRCodeInsideImage,
@@ -11,34 +9,14 @@ import { Center, Stack } from '@/shared/components/common/Layout';
 import { QRCode } from '@/shared/components/common/QRCode';
 
 export const ArAssetDetail = () => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const handleDownload = useCallback(() => {
-    if (!ref.current) return;
-
-    toPng(ref.current, { cacheBust: true })
-      .then((dataUrl) => {
-        const link = document.createElement('a');
-        link.download = 'my-image-name.png';
-        link.href = dataUrl;
-        link.click();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [ref]);
-
   return (
     <Container p={0}>
       <Center bg="blue.1" h={350}>
-        <button onClick={handleDownload}>DOWNLOAD</button>
-        <div ref={ref}>
-          <QRCode
-            url="https://airship.com"
-            imagesrc="/airship-logo-column.svg"
-            size={224}
-          />
-        </div>
+        <QRCode
+          url="https://airship.com"
+          imagesrc="/airship-logo-column.svg"
+          size={224}
+        />
       </Center>
       <Stack p={32} gap={8}>
         <QRCodeInsideImage />
