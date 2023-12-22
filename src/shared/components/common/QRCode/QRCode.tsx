455c638fb32a09626ec18f81086442321b26edc0
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-22 14:35:16 +0900

    feat: ARアセット登録画面でQRコードを保存できるように

--- src/shared/components/common/QRCode/QRCode.tsx
+++ src/shared/components/common/QRCode/QRCode.tsx
@@ -1,4 +1,5 @@
 import { QRCodeSVG } from 'qrcode.react';
+import { RefObject, useState } from 'react';
 import { Center, Paper } from '../Layout';
 import { AspectRatio } from '../Layout/AspectRatio';
 
@@ -7,12 +8,17 @@ type Props = {
   size: number;
   imagesrc?: string;
   style?: React.CSSProperties;
+  qrRef?: RefObject<HTMLDivElement>;
 };
 export const QRCode = (props: Props) => {
-  const { url, imagesrc: imageSrc, size } = props;
+  const { url, imagesrc: imageSrc, size, qrRef } = props;
+
+  const [src, setSrc] = useState(imageSrc ?? '/airship-logo-column.svg');
+
   return (
     <Center {...props}>
       <Paper
+        ref={qrRef}
         radius="md"
         p="md"
         pb={10}
@@ -24,9 +30,12 @@ export const QRCode = (props: Props) => {
             bgColor={'#ffffff'}
             fgColor={'#000000'}
             level={'L'}
+            onError={() => {
+              setSrc('/airship-logo-column.svg');
+            }}
             {...(imageSrc && {
               imageSettings: {
-                src: imageSrc,
+                src: src,
                 x: undefined,
                 y: undefined,
                 height: size / 4.5,
