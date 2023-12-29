86da3bd70569c7a2f7acebfe3b427aa733f7bd44
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-29 15:05:30 +0900

    hotfix: 応急処置として環境変数にデフォルト値を設定

--- src/shared/components/features/QRCode/helper.ts
+++ src/shared/components/features/QRCode/helper.ts
@@ -2,7 +2,8 @@ import { ROUTES } from '@/shared/constants';
 
 // getQRCodeUrlはARアセットのIDをもとにQRコードのURLを生成する関数です。
 export const getQRCodeUrl = (arAssetId: string) => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL || 'https://airship.azurewebsites.net';
   const path = ROUTES.arAssets.public(arAssetId);
 
   return `${baseUrl}${path}`;
