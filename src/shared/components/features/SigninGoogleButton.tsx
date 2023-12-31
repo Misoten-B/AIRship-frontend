1c5c61cdba408bdcef39c577adad36966f981072
hkiku482 <h.kikuchi482@gmail.com>
2023-12-31 11:57:55 +0000

    fix: remove unused exp

--- src/shared/components/features/SigninGoogleButton.tsx
+++ src/shared/components/features/SigninGoogleButton.tsx
@@ -1,21 +1,16 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
-import { useRecoilValue } from 'recoil';
 import { GoogleButton } from '../common/Button';
 import { ErrorNotificationData, notifications } from '../common/Feedback';
 import { useAuth } from '@/shared/hooks/auth';
 import { useCreateUser } from '@/shared/hooks/restapi/v1/User';
-import { useApiClient } from '@/shared/lib/axios/AxiosProvider';
-import { firebaseUserState } from '@/shared/lib/recoil';
 import { useLoading } from '@/shared/providers/loading';
 
 export const SigninGoogleButton = () => {
   const router = useRouter();
   const { createUser } = useCreateUser();
   const { loginWithGoogle, logout } = useAuth();
-  const { api } = useApiClient();
-  const firebaseUser = useRecoilValue(firebaseUserState);
   const { open, close } = useLoading();
 
   const handleClick = useCallback(async () => {
