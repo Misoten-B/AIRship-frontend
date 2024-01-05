98c4c8f26069aa07ad7eb32c9fb1eb83410cac98
honoka2001 <honoka519pk@gmail.com>
2024-01-05 23:34:28 +0900

    add: 概要のスタイル

---
+++ src/app/introduce/_components/IntroducePage.tsx
@@ -0,0 +1,9 @@
+import { Overview } from './Overview/Overview';
+
+export const IntroducePage = () => {
+  return (
+    <>
+      <Overview />
+    </>
+  );
+};
---
+++ src/app/introduce/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { IntroducePage } from './_components/IntroducePage';
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <IntroducePage />
+    </>
+  );
+};
+
+export default Page;
