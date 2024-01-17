94fc766b902b31a4fafdf464fc371c562f81cd56
honoka2001 <honoka519pk@gmail.com>
2024-01-18 03:36:39 +0900

    refactor: スマホサイズの時に見やすいように修正、リファクタ

--- src/app/ar-assets/_components/ArAssetsPage.tsx
+++ src/app/ar-assets/_components/ArAssetsPage.tsx
@@ -6,47 +6,41 @@ import { Image } from '@/shared/components/common/Image';
 import { Center, Grid } from '@/shared/components/common/Layout';
 import { Anchor } from '@/shared/components/common/Navigation';
 import { ROUTES } from '@/shared/constants';
+import { useMediaQuery } from '@/shared/lib/mantine';
 
 export const ArAssetsPage = () => {
+  const isPC = useMediaQuery('(min-width: 768px)');
+
+  const imagePaths = {
+    record: isPC ? '/step/record-pc.svg' : '/step/record.svg',
+    qrcode: isPC ? '/step/qrcode-pc.svg' : '/step/qrcode.svg',
+    card: isPC ? '/step/card-pc.svg' : '/step/card.svg',
+  };
+
+  const steps = [
+    { href: ROUTES.record.base, src: imagePaths.record, alt: 'record' },
+    { href: ROUTES.arAssets.create, src: imagePaths.qrcode, alt: 'qrcode' },
+    { href: ROUTES.cards.create, src: imagePaths.card, alt: 'card' },
+  ];
+
   return (
     <Container p={0}>
-      {/* <Flex
-        gap="xs"
-        p={12}
-        m={12}
-        styles={{
-          root: { borderRadius: '12px' },
-        }}
-      > */}
       <Grid pb="md">
-        <Grid.Col span={4}>
-          <Anchor component={Link} href={ROUTES.record.base} className="step">
-            <Image src="/step/record.svg" alt="record" />
-          </Anchor>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Anchor
-            component={Link}
-            href={ROUTES.arAssets.create}
-            className="step"
-          >
-            <Image src="/step/qrcode.svg" alt="qrcode" />
-          </Anchor>
-        </Grid.Col>
-        <Grid.Col span={4}>
-          <Anchor component={Link} href={ROUTES.cards.create} className="step">
-            <Image src="/step/card.svg" alt="card" />
-          </Anchor>
-        </Grid.Col>
+        {steps.map((step, index) => (
+          <Grid.Col key={index} span={4}>
+            <Anchor component={Link} href={step.href}>
+              <Image src={step.src} alt={step.alt} className="step" />
+            </Anchor>
+          </Grid.Col>
+        ))}
       </Grid>
       <style>{`
         .step:hover {
-          transition : .2s;
-          background: #00000008;
-          borderRadius: '18px'
+          transition: 0.2s;
+          background-color: #00000010;
+          border-radius: 18px;
         }
       `}</style>
-      {/* </Flex> */}
       <Center>
         <ArAssetList />
       </Center>
