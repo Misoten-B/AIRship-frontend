31bd2987695250c9536e8b683610f2e87df45752
mitsuYashi <hm20011027@gmail.com>
2024-01-18 02:50:50 +0900

    change: grid

--- src/app/ar-assets/_components/ArAssetsPage.tsx
+++ src/app/ar-assets/_components/ArAssetsPage.tsx
@@ -1,28 +1,30 @@
+'use client';
 import Link from 'next/link';
 import { ArAssetList } from './ArAssetList';
 import { Container } from '@/shared/components/common/Container';
 import { Image } from '@/shared/components/common/Image';
-import { Center, Flex, Group } from '@/shared/components/common/Layout';
+import { Center, Grid } from '@/shared/components/common/Layout';
 import { Anchor } from '@/shared/components/common/Navigation';
 import { ROUTES } from '@/shared/constants';
 
 export const ArAssetsPage = () => {
   return (
     <Container p={0}>
-      <Flex
+      {/* <Flex
         gap="xs"
         p={12}
         m={12}
         styles={{
           root: { borderRadius: '12px' },
         }}
-      >
-        <Group>
+      > */}
+      <Grid pb="md">
+        <Grid.Col span={4}>
           <Anchor component={Link} href={ROUTES.record.base} className="step">
             <Image src="/step/record.svg" alt="record" />
           </Anchor>
-        </Group>
-        <Group>
+        </Grid.Col>
+        <Grid.Col span={4}>
           <Anchor
             component={Link}
             href={ROUTES.arAssets.create}
@@ -30,20 +32,21 @@ export const ArAssetsPage = () => {
           >
             <Image src="/step/qrcode.svg" alt="qrcode" />
           </Anchor>
-        </Group>
-        <Group>
+        </Grid.Col>
+        <Grid.Col span={4}>
           <Anchor component={Link} href={ROUTES.cards.create} className="step">
             <Image src="/step/card.svg" alt="card" />
           </Anchor>
-        </Group>
-        <style>{`
+        </Grid.Col>
+      </Grid>
+      <style>{`
         .step:hover {
           transition : .2s;
           background: #00000008;
           borderRadius: '18px'
         }
       `}</style>
-      </Flex>
+      {/* </Flex> */}
       <Center>
         <ArAssetList />
       </Center>
