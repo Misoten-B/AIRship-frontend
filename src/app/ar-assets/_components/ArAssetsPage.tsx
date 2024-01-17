03721d3ec8108b4d632ed9cfce3d689475adc813
honoka2001 <honoka519pk@gmail.com>
2024-01-18 02:30:31 +0900

    add: 名刺制作手順がわかりやすいように追加

--- src/app/ar-assets/_components/ArAssetsPage.tsx
+++ src/app/ar-assets/_components/ArAssetsPage.tsx
@@ -1,49 +1,49 @@
 import Link from 'next/link';
 import { ArAssetList } from './ArAssetList';
-import { Button } from '@/shared/components/common/Button';
 import { Container } from '@/shared/components/common/Container';
-import { Center, Flex, Stack } from '@/shared/components/common/Layout';
-import { Text } from '@/shared/components/common/Text';
+import { Image } from '@/shared/components/common/Image';
+import { Center, Flex, Group } from '@/shared/components/common/Layout';
+import { Anchor } from '@/shared/components/common/Navigation';
 import { ROUTES } from '@/shared/constants';
 
 export const ArAssetsPage = () => {
   return (
     <Container p={0}>
-      <Center>
-        <Flex gap={8} px={24} py={12} mb={16}>
-          <Stack gap={0}>
-            <Text size="xs" c="orange" ta="center" mb={4}>
-              3Dモデルと話させる文章を登録して QRコード化させます
-            </Text>
-            <Button
-              color="orange"
-              component={Link}
-              href={ROUTES.arAssets.create}
-              radius="xl"
-              size="md"
-              variant="filled"
-              w="100%"
-            >
-              <Text>QRコードの新規作成</Text>
-            </Button>
-          </Stack>
-          <Stack gap={0}>
-            <Text size="xs" c="blue" ta="center" mb={4}>
-              5秒ほど自分の声を登録するとAI化されます
-            </Text>
-            <Button
-              variant="filled"
-              size="md"
-              radius="xl"
-              w="100%"
-              component={Link}
-              href={ROUTES.record.base}
-            >
-              <Text>声を登録する</Text>
-            </Button>
-          </Stack>
-        </Flex>
-      </Center>
+      <Flex
+        gap="xs"
+        p={12}
+        m={12}
+        styles={{
+          root: { borderRadius: '12px' },
+        }}
+      >
+        <Group>
+          <Anchor component={Link} href={ROUTES.record.base} className="step">
+            <Image src="/step/record.svg" alt="record" />
+          </Anchor>
+        </Group>
+        <Group>
+          <Anchor
+            component={Link}
+            href={ROUTES.arAssets.create}
+            className="step"
+          >
+            <Image src="/step/qrcode.svg" alt="qrcode" />
+          </Anchor>
+        </Group>
+        <Group>
+          <Anchor component={Link} href={ROUTES.cards.create} className="step">
+            <Image src="/step/card.svg" alt="card" />
+          </Anchor>
+        </Group>
+        <style>{`
+        .step:hover {
+          transition : .2s;
+          background: #00000008;
+          borderRadius: '18px'
+        }
+      `}</style>
+      </Flex>
       <Center>
         <ArAssetList />
       </Center>
